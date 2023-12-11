40d1d081ebfe9b2f6d28967052a21f731e006ac3
panini-19 <nianthony195@gmail.com>
2023-12-11 18:08:43 -0500

    updated some code

--- src/ToDoList/ToDoList.tsx
+++ src/ToDoList/ToDoList.tsx
@@ -3,14 +3,16 @@ import './ToDoList.css';
 
 const TaskList = () => {
     const [toDo, setToDo] = useState('');
+    // specifying the itmes are strings
     const [items, setItems] = useState<string[]>([]);
-
+    // 'ChangeEvent' makes sure the list gets updated everytime an item is added
     const handleUI = (event: React.ChangeEvent<HTMLInputElement>) => {
         setToDo(event.target.value);
     }
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        // .trim checks to make sure empty string is not inputted
         if (toDo.trim() !== '') {
             setItems([...items, toDo]);
             setToDo('');
@@ -19,6 +21,9 @@ const TaskList = () => {
 
     const removeItem = (index: number) => {
         const updatedItems = [...items];
+        // splicing updates the item in the array by removing it at the specified index
+        // the value of '1' makes sure only 1 item is moved at a time
+        // maybe can add button in the future to remove all tasks 
         updatedItems.splice(index, 1);
         setItems(updatedItems);
     }
